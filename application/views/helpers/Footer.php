4130dc971ac924a967bdfddf34f219e6e6fd47d4
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-10-07 09:58:46 +0200

    Some small updates

--- application/views/helpers/Footer.php
+++ application/views/helpers/Footer.php
@@ -9,10 +9,7 @@ class Zend_View_Helper_Footer extends Zend_View_Helper_Abstract
             $year = "2010, $year";
         }
         return <<<EOT
-    <p>Copyright &copy; $year by <a href="http://akrabat.com">Rob Allen</a>.<br/>
-    This application is built with <a href="http://framework.zend.com">Zend Framework</a>.
-    Source code is available on <a href="http://www.github.com/akrabat/TweetGT">github</a> 
-    and licensed under the <a href="http://akrabat.com/license/new-bsd/">New BSD</a>.
+    <p>Copyright &copy; $year </a>.
 
 
 EOT;
